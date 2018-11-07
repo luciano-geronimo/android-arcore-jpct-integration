0b834c5a772cfe2a9277290623928bdaf2373116
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2018-11-06 22:16:12 -0200

    Implemented the JPCT's "Hello World" based on http://www.jpct.net/wiki/index.php/Hello_World_for_Android . Beware the framebuffer constructor. Must use the one with only width and height instead of the one with GL, width, height. If you dont use the correct constructor the system will crash with an array error deep in the render method.

--- app/src/main/java/arcorejpct/geronimo/don/arcorewithjpct_ae/activities/MainActivity.kt
+++ app/src/main/java/arcorejpct/geronimo/don/arcorewithjpct_ae/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import android.opengl.GLSurfaceView
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import arcorejpct.geronimo.don.arcorewithjpct_ae.R
+import arcorejpct.geronimo.don.arcorewithjpct_ae.utils.DisplayRotationHelper
 import arcorejpct.geronimo.don.arcorewithjpct_ae.utils.FullscreenHelper
 import arcorejpct.geronimo.don.arcorewithjpct_ae.utils.GLSurfaceViewConfigurer
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,12 +13,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     /**The implementator of the rendering contract*/
     private lateinit var  renderer : MainActivityOpenGLRenderer
+    private lateinit var displayRotationHelper : DisplayRotationHelper
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        renderer = MainActivityOpenGLRenderer(this)
+        displayRotationHelper = DisplayRotationHelper(this)
+        renderer = MainActivityOpenGLRenderer(this, displayRotationHelper)
         GLSurfaceViewConfigurer.openGlSetup(renderer, surfaceView)
+
 
     }
 
@@ -26,12 +31,14 @@ class MainActivity : AppCompatActivity() {
         //todo a lot of sorcery
         //resumes the gl suface. Order matters
         surfaceView.onResume() //must be paused after the arcore session
+        displayRotationHelper.onResume()
     }
 
     override fun onPause() {
         super.onPause()
         //todo a lot of sorcery
         //pauses the gl surface. Order matters
+        displayRotationHelper.onPause()
         surfaceView.onPause() //must be paused before the arcore Session
     }
 
--- app/src/main/java/arcorejpct/geronimo/don/arcorewithjpct_ae/activities/MainActivityOpenGLRenderer.kt
+++ app/src/main/java/arcorejpct/geronimo/don/arcorewithjpct_ae/activities/MainActivityOpenGLRenderer.kt
@@ -3,6 +3,7 @@ package arcorejpct.geronimo.don.arcorewithjpct_ae.activities
 import android.content.Context
 import android.opengl.GLSurfaceView
 import arcorejpct.geronimo.don.arcorewithjpct_ae.R
+import arcorejpct.geronimo.don.arcorewithjpct_ae.utils.DisplayRotationHelper
 import com.threed.jpct.*
 import com.threed.jpct.util.BitmapHelper
 import com.threed.jpct.util.MemoryHelper
@@ -11,9 +12,10 @@ import javax.microedition.khronos.opengles.GL10
 /**
  * Implements the rendering contract.
  * */
-class MainActivityOpenGLRenderer(_ctx:Context) : GLSurfaceView.Renderer {
+class MainActivityOpenGLRenderer(_ctx:Context, displayRotHelper:DisplayRotationHelper) : GLSurfaceView.Renderer {
     private val context = _ctx
-    private lateinit var frameBuffer: FrameBuffer
+    private val displayRotationHelper = displayRotHelper
+    private var frameBuffer: FrameBuffer? = null
 
     private var isSceneInitialized : Boolean = false
     private lateinit var world : World
@@ -21,37 +23,41 @@ class MainActivityOpenGLRenderer(_ctx:Context) : GLSurfaceView.Renderer {
     private lateinit var cube : Object3D
 
     override fun onDrawFrame(gl: GL10?) {
+        //displayRotationHelper.updateSessionIfNeeded(session) TODO so vai funcionar quando tiver a session
         val backgroundColor = RGBColor(100,40, 40)
-        frameBuffer.clear(backgroundColor)
+        frameBuffer?.clear(backgroundColor)
         world.renderScene(frameBuffer)
         world.draw(frameBuffer)
-        frameBuffer.display()
-//        TODO Render something
+        frameBuffer?.display()
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        displayRotationHelper.onSurfaceChanged(width,height)
+
         frameBuffer?.dispose()
 
         frameBuffer = FrameBuffer(width, height)// FrameBuffer(gl,width,height)
         if (!isSceneInitialized) {
             world = World()
+            //TODO ambient light will come from Arcore
             world.setAmbientLight(20,20,20)
             val sun = Light(world)
             sun.setIntensity(250.0f, 250.0f, 250.0f)
             val texture = Texture(BitmapHelper.rescale(BitmapHelper.convert( context.resources.getDrawable(R.mipmap.ic_launcher)),64,64));
             val textureName = "foobar"
+            //TODO the cube's position and orientation will come from jpct
             TextureManager.getInstance().addTexture(textureName, texture)
-             cube = Primitives.getCube(10.0f)
+            cube = Primitives.getCube(10.0f)
             cube.calcTextureWrapSpherical()
             cube.setTexture(textureName)
             cube.strip()
             cube.build()
             world.addObject(cube)
-
+            //TODO Will i be able to pass the view and projection matrixes that Arcore will give me to JPCT camera or will i have to implement my own camera?
             camera = world.camera
             camera.moveCamera(Camera.CAMERA_MOVEOUT, 50.0f)
             camera.lookAt(cube.transformedCenter)
-
+            //TODO most probably there won't be a sun in the Arcore scene.
             val sv = SimpleVector()
             sv.set(cube.transformedCenter)
             sv.y -= 100
@@ -61,10 +67,10 @@ class MainActivityOpenGLRenderer(_ctx:Context) : GLSurfaceView.Renderer {
             MemoryHelper.compact()
             isSceneInitialized = true
         }
-//        TODO create something to render
+
     }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //Not using for now.
     }
 }
